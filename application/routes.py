beeff0f6df313307166088bc052b4907f6ebbd54
Borys Więsek <lordboria.boryson@gmail.com>
2024-11-03 13:14:25 +0100

    mongodb integration, created classes, added users endpoint

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
-from application import app
+from application import app, db
 from flask import render_template, request, json, Response
+from application.models import User, Course, Enrollment
 
 course_data = [
     {"courseID": "1111", "title": "PHP 101", "description": "Intro to PHP", "credits": 3, "term": "Fall, Spring"},
@@ -52,3 +53,8 @@ def api(idx=None):
         jdata = course_data[int(idx)]
 
     return Response(json.dumps(jdata), mimetype="application/json")
+
+@app.route("/user")
+def user():
+    users = User.objects.all()
+    return render_template("user.html", users=users)
